f21cc75389c2f1e8bb4650691eab702b598b6b31
lalawue <suchaaa@gmail.com>
2020-01-23 00:29:44 +0800

    add interface to get escape content

--- src/htmlparser/ElementNode.lua
+++ src/htmlparser/ElementNode.lua
@@ -129,6 +129,14 @@ function ElementNode:getcontent()
 	return string.sub(self.root._text, self._openend + 1, self._closestart - 1)
 end
 
+function ElementNode:getescapecontent()
+        local n = self
+        while #n.nodes > 0 do
+                n = n.nodes[1]
+        end
+        return n:getcontent()
+end
+
 function ElementNode:addattribute(k, v)
 	self.attributes[k] = v
 	if string.lower(k) == "id" then
